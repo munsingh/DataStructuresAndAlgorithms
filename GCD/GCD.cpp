fc3fd7e6b7fa3f8b620bc66928066a2a983b4f70
Manish Singh <munsingh@hotmail.com>
2019-03-10 13:34:15 +0530

    optimized algorithm using Euclid Algorithm to find Greatest Common Divisor.

--- GCD/GCD.cpp
+++ GCD/GCD.cpp
@@ -3,17 +3,14 @@
 
 long long GCD(long long lNumber1, long long lNumber2) {
 	//GCD of two numbers is a number, which divides both the numbers Number1 and Number2
-	//This simple alogrithm we start a infinite loop wit number starting from 2
-	//and we start dividing both the numbers number1 and number2 by the number. 
-	//If both are completely divisible then we break out of the loop otherwise we continue
+	//We will the Euclid algorithm to find the answer
+	//wherein if a' is the remainder when number1 is dividied by number2
+	//then d is the GCD if d divides a' as well as b
+	if (lNumber2 == 0)
+		return lNumber1;
 
-	long long lGCD = 2;
-	for ( long long lGCD = 2 ; lGCD < lNumber1 + lNumber2; ++lGCD ) {
-		if (lNumber1 % lGCD == 0 && lNumber2 % lGCD == 0)
-			break;
-	}
-
-	return lGCD;
+	long long nNumberPrime = lNumber1 % lNumber2;
+	return GCD(lNumber2, nNumberPrime );
 }
 
 int main(int nArgv, char** pArgc) {
@@ -21,7 +18,12 @@ int main(int nArgv, char** pArgc) {
 	long long lNumber1(0), lNumber2(0);
 	std::cin >> lNumber1;
 	std::cin >> lNumber2;
+	
+	if (lNumber1 > lNumber2) {
+		std::cout << GCD(lNumber1, lNumber2);
+	}
+	else 
+		std::cout << GCD( lNumber2, lNumber1 );
 
-	std::cout << GCD(lNumber1, lNumber2);
 	return 0;
 }
